a34b2176c650eedce4287e1c1316adc62e13077a
SoftwareSmeden <torb815g@edu.easj.dk>
2021-09-08 18:23:03 +0200

    1.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,16 +10,19 @@ public class Main {
         String month = "September";
         int date = 03;
         int year = 2021;
+
         printAmerican(day, month, date, year);
         printEuropean(day, date, month, year);
 
     }
+        //Metode 1:
     public static void printAmerican(String day, String month, int date, int year) {
         System.out.print("American format: " + day + "," + " " + month + " " + date + "," + " " + year + "\n");
 
     }
+        // Metode 2:
     public static void printEuropean(String day, int date, String month, int year) {
-            System.out.print("European format: " + day + "," + " " + date + " " + month + "," + " " + year);
+        System.out.print("European format: " + day + "," + " " + date + " " + month + "," + " " + year);
 
     }
 }
